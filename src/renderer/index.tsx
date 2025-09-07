f439ca6dc83ae2b54084dd80b955e93cbf2923b5
Jason Skollingsberg <dragonleech@gmail.com>
2025-09-06 20:12:38 -0400

    feat: Integrate Redux Provider into React application

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
 import App from './App_Complete';
 
 const container = document.getElementById('root');
@@ -8,4 +9,8 @@ if (!container) {
 }
 
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
